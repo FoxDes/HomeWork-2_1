17ff9f88b092f45690d6967e580e5f3b89de650d
Denis Malyarenko <mdn-fox@yandex.ru>
2022-09-24 16:16:25 +0500

    выполнил первую задачу по дз

---
+++ seminar1.py
@@ -0,0 +1,43 @@
+# print('Введите число: ')
+# a = int(input())
+# print('Введите второе число: ')
+# b = int(input())
+# if a * a == b or b * b == a:
+#     print('Yes')
+# else:
+#     print('No')
+
+
+# max_var = 0
+
+# for i in range(5):
+#     a = int(input())
+#     if a > max_var:
+#         max_var = a
+# print(max_var)
+
+
+# n = int(input())
+# print(*list(range(-n, n + 1)))
+
+
+# a = float(input('Введите число: '))
+# a = a * 10 % 10
+# if a != 0:
+#     print(int(a))
+# else:
+#     print('No')
+
+
+# a = int(input('Введите N: '))
+# if (a % 5 == 0 and a % 10 == 0 or a % 15 == 0) and a % 30 != 0:
+#     print(int(a))
+# else:
+#     print('No')
+
+
+# for x in range(2):
+#     for y in range(2):
+#         for z in range(2):
+#             if not (x == z or x <= y and z):
+#                 print(x, y, z)
